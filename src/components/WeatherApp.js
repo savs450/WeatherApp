0761cb0aaa2f95ed8bbf7196099d8473f8101fbf
Savita chaudhary <savs2728@gmail.com>
2024-04-26 23:09:31 +0530

    final changes done

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,23 +1,118 @@
-import React,{useState, useEffect} from 'react'
-import SearchIcon from '@mui/icons-material/Search';
-import './weather.css'
+import React, { useState, useEffect } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import "./weather.css";
+import axios from "axios";
+import Weathercard from "./WeatherCard";
+import clear from "../asset/BackgroundImages/Clear.jpg";
+import cloudy from "../asset/BackgroundImages/Cloudy.jpg";
+import fog from "../asset/BackgroundImages/fog.png";
+import rainy from "../asset/BackgroundImages/Rainy.jpg";
+import snowy from "../asset/BackgroundImages/snow.jpg";
+import stormy from "../asset/BackgroundImages/Stormy.jpg";
+import sunny from "../asset/BackgroundImages/Sunny.jpg";
 
 function WeatherApp() {
-    const [inputVal , setInputVal ]= useState([])
+  const [inputVal, setInputVal] = useState([]);
+  const [weatherData, setWeatherData] = useState(null);
+  const [filterObj, setFilterObj] = useState(null);
+  const [background, setBackground] = useState(sunny);
+  const apiKey = "fd1b03aaa923a18074d819a1fe44f8f2";
 
+  async function fetchData() {
+    // console.log("fetchData is calling..");
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&units=Metric&appid=${apiKey}`;
+    if (inputVal.trim() !== "") {
+      try {
+        let response = await axios.get(url);
+        setWeatherData(response.data);
+        // console.log("response", response.data);
+      } catch (err) {
+        console.warn("something went wrong", err);
+      }
+    }
+    setInputVal("");
+  }
+
+  useEffect(() => {
+    if (weatherData !== null) {
+      const { main, name, weather, wind } = weatherData;
+      setFilterObj({
+        temperature: main.temp,
+        windSpeed: wind.speed,
+        humidity: main.humidity,
+        place: name,
+        heatIndex: main.feels_like,
+        weather: weather[0].main,
+        conditions: weather[0].description,
+      });
+      switch (weather[0].main) {
+        case "Clear":
+          setBackground(clear);
+          break;
+        case "Clouds":
+        case "Haze":
+          setBackground(cloudy);
+          break;
+        case "Rain":
+          setBackground(rainy);
+          break;
+        case "Snow":
+          setBackground(snowy);
+          break;
+        case "Storm":
+          setBackground(stormy);
+          break;
+        default:
+          setBackground(sunny);
+      }
+    }
+  }, [weatherData]);
+
+ 
+  //   handleWeatherImages(conditions);
+  // }, [conditions]);
+
+  // const handleWeatherBackground = (weather) => {
+  //   switch (weather) {
+  //     case "Clear":
+  //       setBackground(sunny);
+  //       return;
+  //     case "Clouds":
+  //     case "Haze":
+  //       setBackground(cloudy);
+  //       return;
+  //     case "Rain":
+  //       setBackground(rainy);
+  //       return;
+  //     case "Snow":
+  //       setBackground(snowy);
+  //       return;
+  //     case "Storm":
+  //       setBackground(stormy);
+  //       return;
+  //     default:
+  //       setBackground(clear);
+  //       return;
+  //   }
+  // };
   return (
-    <div>
-        <h2 className='heading'>Weather Application</h2>
-       <div className='search_input'>
-        <input placeholder='Search city....'
-        type='text'
-        value={inputVal}
-        onChange={(e)=>setInputVal(e.target.value)}
+    <div className="main_conatiner" style={{ backgroundImage: `url(${background})` }}>
+      <h1 className="heading">Weather Application</h1>
+      <div className="search_input">
+        <input
+          placeholder="Search city...."
+          type="text"
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+          onKeyDown={(e) => (e.key === "Enter" ? fetchData() : "")}
         ></input>
-        <span><SearchIcon/></span>
-       </div>
+        <span>
+          <SearchIcon />
+        </span>
+      </div>
+     {filterObj !== null && <Weathercard obj={filterObj} />}
     </div>
-  )
+  );
 }
 
-export default WeatherApp
+export default WeatherApp;
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react";
+import "./weather.css";
+import {useDate} from '../utils/time'
+
+import cloud from "../asset/weatherIcons/cloud.png";
+import fog from "../asset/weatherIcons/fog.png";
+import rain from "../asset/weatherIcons/rain.png";
+import snow from "../asset/weatherIcons/snow.png";
+import storm from "../asset/weatherIcons/storm.png";
+import sun from "../asset/weatherIcons/sun.png";
+import windy from "../asset/weatherIcons/windy.png";
+
+function Weathercard({ obj }) {
+  const [icon, setIcon] = useState(sun);
+
+
+  const {
+    place,
+    temperature,
+    windSpeed,
+    humidity,
+    heatIndex,
+    conditions,
+    weather
+  } = obj;
+
+  useEffect(() => {
+    handleWeatherImages(weather);
+  }, [weather]);
+
+  const handleWeatherImages = (weather) => {
+    switch (weather) {
+      case "Clear":
+        setIcon(sun);
+        return;
+      case "Clouds":
+      case "Haze":
+        setIcon(cloud);
+        return;
+      case "Rain":
+        setIcon(rain);
+        return;
+      case "Snow":
+        setIcon(snow);
+        return;
+      case "Storm":
+        setIcon(storm);
+        return;
+      default:
+        setIcon(sun);
+        return;
+    }
+  };
+  function capitialize(str){
+    let result = str.split(' ').map(word => word.charAt(0).toUpperCase()+ word.slice(1)).join(' ')
+    return result
+  }
+
+
+let currentDate = new Date();
+let hours = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+let ampm = hours >= 12 ? 'PM' : 'AM';
+
+hours = hours % 12;
+hours = hours ? hours : 12; 
+
+minutes = minutes < 10 ? '0' + minutes : minutes;
+let currentTime = hours + ':' + minutes + ' ' + ampm;
+
+  return (
+    <div>
+      <div className="weatherCard">
+        <div className="temperature">
+          <img src={icon} alt={weather}></img>
+          <h2>{temperature}&deg;C</h2>
+        </div>
+        <p className="city">{place}</p>
+        <div className="date">
+          <p>{new Date().toDateString()}</p>
+          <p>{currentTime}</p>
+        </div>
+        <div className="other_details">
+          <div className="windSpeed">
+            <span>WindSpeed</span>
+            <span>{windSpeed}km/h</span>
+          </div>
+          <div className="humidity">
+            <span>Humidity</span>
+            <span>{humidity}</span>
+          </div>
+        </div>
+        <div className="heatIndex">
+          <p>Heat Index</p>
+          <p>{heatIndex}</p>
+        </div>
+        <div className="border"></div>
+        <div className="condition">{capitialize(conditions)}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Weathercard;
